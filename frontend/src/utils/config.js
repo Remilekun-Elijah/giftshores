09a07ddd5accbe86a16c4c9b49ee8275afb6c112
= <=>
2023-12-17 22:51:52 +0100

    updates backend url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -28,8 +28,8 @@ environment.staging = {
 environment.production = {
   appUser: import.meta.env.VITE_APP_USER,
   authProps: ["g/token", "g/user"],
-  backendUrl: "https://api.giftshores.com/v1/",
-  // backendUrl: "https://fine-gray-dog-gown.cyclic.app/v1/",
+  // backendUrl: "https://api.giftshores.com/v1/",
+  backendUrl: "https://fine-gray-dog-gown.cyclic.app/v1/",
   routes,
   frontendUrl: "https://www.giftshores.com/",
 };
